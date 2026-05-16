8245bb0d1549c1147089bb3778142b5114640dee
Jeshu Patel <jeshupatelg3774@gmail.com>
2026-05-17 01:57:42 +0530

    failsafe network create

--- docker/network-compose.sh
+++ docker/network-compose.sh
@@ -1,3 +1,4 @@
-#bin/bash
+#!/bin/bash
 
-docker network create gateway_net
+# Create the network only if it doesn't already exist
+docker network inspect gateway_net >/dev/null 2>&1 || docker network create gateway_net
